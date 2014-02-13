5330471452c8ad21d64514ebea9dba5c212b78f2
cloudmonkeypeng <cloudmonkeypeng@live.com>
2014-02-13 12:02:34 +0800

    Revert "fix the  iframe bug"
    This reverts commit ea18a715317d0f32cd5afd5f0667439cb88a07a0.

--- appjs/WindowFrame.js
+++ appjs/WindowFrame.js
@@ -1,63 +1,199 @@
- Ext.define('MyDesktop.WindowFrame', {
+Ext.define('MyDesktop.WindowFrame', {
 
-    extend: 'Ext.ux.desktop.Module',
+ 	extend: 'Ext.ux.desktop.Module',
 
-    requires : [ 'Ext.ux.IFrame'],
-    
-    id:null,
-    
-    config:null,
-    
-    constructor: function (config) {
+    requires: ['Ext.Viewport'],
+
+    id:null, 
+
+    config:null, 
+
+    constructor:function(config){
         var me = this;
-        me.id=config.id;
+        me.id = config.id;
 
-        me.config=config;
-        me.callParent();  
+        me.config = config;
+        me.callParent();
     },
 
-    init : function(){
-        this.launcher = {
-            text: this.config.text,
-            iconCls: this.config.smallIcon
+    init:function(){
+        this.launcher ={
+            text : this.config.text,
+            iconCls : this.config.smallIcon
         };
+
+       
     },
+
+  /*  onReady:function(){
+        var cw;
+
+        Ext.state.Manager.setProvider(Ext.create('Ext.state.CookieProvider'));
+
+        // function closeRegion (e, target, header, tool) {
+        //     var panel = header.ownerCt;
+        //     newRegions.unshift(panel.initialConfig);
+        //     viewport.remove(panel);
+        // }
+
+        var newRegions = [{
+                region: 'north',
+                title: 'North 2',
+                height: 100,
+                collapsible: true,
+                weight: -120
+            }, {
+                region: 'east',
+                title: 'East 2',
+                width: 100,
+                collapsible: true,
+                weight: -110
+            }, {
+                region: 'west',
+                title: 'West 2',
+                width: 100,
+                collapsible: true,
+                weight: -110
+        }];
+    },*/
+
 
     createWindow: function(){
-        
-        var desktop = this.app.getDesktop();
-        var win = desktop.getWindow(this.config.id);
+         var cw;
+
+        Ext.state.Manager.setProvider(Ext.create('Ext.state.CookieProvider'));
+
+        // function closeRegion (e, target, header, tool) {
+        //     var panel = header.ownerCt;
+        //     newRegions.unshift(panel.initialConfig);
+        //     viewport.remove(panel);
+        // }
+
+        var newRegions = [{
+                region: 'north',
+                title: 'North 2',
+                height: 100,
+                collapsible: true,
+                weight: -120
+            }, {
+                region: 'east',
+                title: 'East 2',
+                width: 100,
+                collapsible: true,
+                weight: -110
+            }, {
+                region: 'west',
+                title: 'West 2',
+                width: 100,
+                collapsible: true,
+                weight: -110
+        }];
+
+
+    	var desktop = this.app.getDesktop();
+        var win = desktop.getWindow(this.id);
         if(!win){
-            win = desktop.createWindow({
-                width:1100,
-                height:600,
+        	win = desktop.createWindow({
+                width:1000,
+                height:500,
                 title:this.config.text,
-                iconCls:this.config.smallIcon,  
-                id:this.config.id,
-                //html:'<a>放置在线查控</a>',
-                //html:'<iframe src="dept/index.action" width="100%" height="100%" scrolling="yes"><iframe>',
-               // html:'<iframe frameborder="0" src="home2014115.jsp" style="padding:2px"  width="100%" height="100%" scrolling="no"><iframe>',
-               // html:'<iframe frameborder="0" src="'+ this.config.action+'" style="padding:2px"  width="100%" height="100%" scrolling="no"><iframe>',
-                items: [
-                        { 
-                            xtype: 'uxiframe',
-                            layout: 'fit',
-                            src:this.config.url,
-                            width:"100%",
-                            height:"100%",
-                            scrolling:"no",
-                            loadMask: '页面加载中...',
-                        }
-                    ],
+                iconCls:this.config.smallIcon,
+                id:this.id, 
+                html:'<iframe frameborder="0" src="http://localhost/ext-desktop/layout.html" width="100%" height="100%" scrolling="yes"><iframe>',
                 animCollapse:false,
-                constrainHeader:true
+                constrainHeader:true,
+                layout: {
+                    type: 'border',
+                    padding: 5
+                },
+                defaults: {
+                    split: true
+                },
+                /*items: [{
+                    region: 'west',
+                    collapsible: true,
+                    layout: 'absolute',
+                    title: 'Starts at width 30%',
+                    split: true,
+                    width: '30%',
+                    minWidth: 100,
+                    minHeight: 140,
+                    bodyPadding: 10,
+                    stateId: 'westRegion',
+                    stateful: true,
+                    html: '<div style="position:absolute;bottom:10px;right:10px;">'+
+                            'west<br>I am floatable and stateful!</div>',
+                    items: [{
+                        xtype: 'regionsetter'
+                    }]
+                },{
+                    region: 'center',
+                    //html: 'center center',
+                    title: 'Center',
+                    minHeight: 80,
+                }]*/
             });
             win.show();
-        }else{
+         }else{
             win.focus();
         }
         return win;
+    },
+
+
+    afterRender: function () {
+        this.callParent();
+        
+        this.ownerCt.on({
+            changeregion: 'onChangeRegion',
+            scope: this
+        });
+        this.onChangeRegion(this.ownerCt);
+
+        this.el.on({
+            click: this.onClick,
+            mouseover: this.onMouseOver,
+            mouseout: this.onMouseOut,
+            scope: this
+        });
+    },
+
+    onClick: function (e) {
+        var target = Ext.fly(e.getTarget());
+        var region;
+
+        if (target.hasCls('ux-arrow-up')) {
+            region = 'north';
+        } else if (target.hasCls('ux-arrow-left')) {
+            region = 'west';
+        } else if (target.hasCls('ux-arrow-right')) {
+            region = 'east';
+        } else if (target.hasCls('ux-arrow-down')) {
+            region = 'south';
+        }
+
+        if (region && region !== this.ownerCt.region) {
+            this.ownerCt.setBorderRegion(region);
+        }
+    },
+
+    onMouseOver: function (e) {
+        var target = Ext.fly(e.getTarget());
+        if (target.hasCls('ux-arrow')) {
+            target.addCls('ux-arrow-over');
+        }
+    },
+
+    onMouseOut: function (e) {
+        var target = Ext.fly(e.getTarget());
+        target.removeCls('ux-arrow-over');
+    },
+
+    onChangeRegion: function (panel, oldRegion) {
+        if (oldRegion) {
+            this.removeCls('ux-arrow-current-' + oldRegion);
+        }
+        this.addCls('ux-arrow-current-' + panel.region);
+
     }
-
-
- });
+});
